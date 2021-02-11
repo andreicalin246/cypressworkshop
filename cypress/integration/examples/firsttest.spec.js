0956567d4e8625e1bc34d6decb36c119e3e1b784
ANDREIP-PC2\Andrei <andrei.popa@itperspectives.ro>
2021-02-11 15:05:33 +0200

    wpa org Personal

--- cypress/integration/examples/firsttest.spec.js
+++ cypress/integration/examples/firsttest.spec.js
@@ -1,8 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Link acasa', function(){
-    it('Open', function(){
-        cy.visit('https://tantraesoteric.ro/');
-        cy.get('a').contains('Acasă').should('be.visible');
+    it('Desktop menu items are visible', function(){
+        cy.viewport('macbook-16')
+        cy.visit('https://www.wpa.org.uk/');
+        cy.get('#ccc-notify-accept').click();
+        cy.get('a').contains('Personal').should('be.visible');
+
+        
+    })
+    it('Mobile menu items are visible', function(){
+        cy.viewport("iphone-6+")
+        cy.visit('https://www.wpa.org.uk/');
+        cy.get('#ccc-notify-accept').click();
+        cy.get('div.mobile').click();
+        cy.get('nav.mobile-menu > div > div > a').contains("Personal").should("be.visible");
+
+        
     })
 })
